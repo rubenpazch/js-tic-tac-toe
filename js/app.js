954055f10730809aad1401e52687a58f60922ffa
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-06-16 12:14:50 -0500

    add symbol propertie to player

--- js/app.js
+++ js/app.js
@@ -24,8 +24,8 @@ function checkDraw(){
     return false;
 }
 
-const Player = (name, score) => {
-  return { name, score };
+const Player = (name, score, simbol) => {
+  return { name, score, simbol };
 };
 
 function checkWin() {
@@ -115,23 +115,63 @@ function showElement(elementId) {
 window.onload = function() {
   hideElement("board");
   showElement("players");
+  hideElement("reset-top-button");
 };
 
 function updateScores() {
-  document.getElementById("score-player-1").innerHTML = "Score: " + player1.score;
-  document.getElementById("score-player-2").innerHTML = "Score: " + player2.score;
+  document.getElementById("score-player-1").innerHTML =  player1.score;
+  document.getElementById("score-player-2").innerHTML =  player2.score;
 }
 
 function startGame(){
   showElement("board");
-
+  showElement("reset-top-button");
   let namePlayer1 = document.getElementById("PlayerName1");
   let namePlayer2 = document.getElementById("PlayerName2");
-  player1 = Player(namePlayer1.value, 0);  
-  player2 = Player(namePlayer2.value, 0);
+  let symbol = document.getElementById("inputSymbolGame");
+
+  
+
+  if (namePlayer1.value ==""){
+    player1 = Player("player-1", 0);  
+    player2 = Player("player-2", 0);
+  }else {
+    player1 = Player(namePlayer1.value, 0);  
+    player2 = Player(namePlayer2.value, 0);
+  }
+
+  if (symbol.value == 1)
+  {
+    player1.symbol = "X";
+    player2.symbol = "O";
+  }
+  else{
+    player1.symbol = "O";
+    player2.symbol = "X";
+  }
+  console.log(player1);
+  console.log(player2);
+
   document.getElementById("name-player-1").innerHTML = player1.name;
   document.getElementById("name-player-2").innerHTML = player2.name;
   updateScores();
 
   hideElement("players");
+}
+
+function newGame(){
+  cleanCells();
+  hideElement("board");
+  showElement("players");
+  cleanForm();
+  continueGame = true; 
+}
+
+function cleanForm(){
+  document.getElementById("PlayerName1").value ="";
+  document.getElementById("PlayerName2").value ="";
+  document.getElementById("name-player-1").innerHTML = "[name]";
+  document.getElementById("name-player-2").innerHTML = "[name]";
+  document.getElementById("score-player-1").innerHTML = "[score]";
+  document.getElementById("score-player-2").innerHTML = "[score]";
 }
