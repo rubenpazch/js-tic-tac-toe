a85494d468c2609a5d95a21ef0d72692eb2ec253
migueldp4 <migueldp4@gmail.com>
2020-06-16 15:38:15 -0600

    Fix linter issues

--- js/app.js
+++ js/app.js
@@ -3,22 +3,33 @@ let player1;
 let player2;
 let continueGame = true;
 
+const Player = (name, symbol) => {
+  let score = 0;
+  const getScore = () => score;
+  const increaseScore = () => {
+    score += 1;
+  };
+  return {
+    name, symbol, increaseScore, getScore,
+  };
+};
+
 const displayModule = (() => {
   const showMessageDraw = () => {
     const winMessage = document.getElementById('win-message');
     winMessage.innerHTML = 'It\'s a draw';
   };
-  
+
   const showElement = (elementId) => {
     const form = document.getElementById(elementId);
     form.style.display = 'block';
   };
-  
+
   const hideElement = (elementId) => {
     const form = document.getElementById(elementId);
     form.style.display = 'none';
   };
-  
+
 
   const updateScores = () => {
     document.getElementById('score-player-1').innerHTML = player1.getScore();
@@ -49,7 +60,9 @@ const displayModule = (() => {
     element.onclick = action;
   };
 
-  return { showMessageDraw, showElement, updateScores, cleanCells, hideElement,cleanForm, addOnClickEvent }
+  return {
+    showMessageDraw, showElement, updateScores, cleanCells, hideElement, cleanForm, addOnClickEvent,
+  };
 })();
 
 const boardModule = (() => {
@@ -80,37 +93,37 @@ const boardModule = (() => {
     return -1;
   };
 
-  const resetGame = () => {    
+  const resetGame = () => {
     cells = ['', '', '', '', '', '', '', '', ''];
     displayModule.cleanCells();
     continueGame = true;
-    displayModule.hideElement("win-screen");
+    displayModule.hideElement('win-screen');
   };
 
-  const  newGame = () =>  {
+  const newGame = () => {
     displayModule.cleanCells();
     displayModule.hideElement('board');
     displayModule.showElement('players');
     displayModule.cleanForm();
     continueGame = true;
   };
-  
+
   const startGame = () => {
     displayModule.showElement('board');
     displayModule.showElement('reset-top-button');
     const namePlayer1 = document.getElementById('PlayerName1');
     const namePlayer2 = document.getElementById('PlayerName2');
     const symbol = document.getElementById('inputSymbolGame');
-  
-  
+
+
     if (namePlayer1.value === '') {
-      player1 = Player('player-1', "X");
-      player2 = Player('player-2', "O");
+      player1 = Player('player-1', 'X');
+      player2 = Player('player-2', 'O');
     } else {
       player1 = Player(namePlayer1.value);
       player2 = Player(namePlayer2.value);
     }
-  
+
     if (symbol.value === 1) {
       player1.symbol = 'X';
       player2.symbol = 'O';
@@ -118,18 +131,18 @@ const boardModule = (() => {
       player1.symbol = 'O';
       player2.symbol = 'X';
     }
-  
-  
+
+
     document.getElementById('show-symbol-player-1').innerHTML = player1.symbol;
     document.getElementById('show-symbol-player-2').innerHTML = player2.symbol;
-  
+
     document.getElementById('name-player-1').innerHTML = player1.name;
     document.getElementById('name-player-2').innerHTML = player2.name;
     displayModule.updateScores();
-  
+
     displayModule.hideElement('players');
   };
-  
+
   const showMessageWinner = () => {
     const winMessage = document.getElementById('win-message');
     if (turn === player1.symbol) {
@@ -142,53 +155,41 @@ const boardModule = (() => {
     }
   };
 
-
-  return { checkWin, fillBoardCell, checkDraw, resetGame, newGame, startGame, showMessageWinner };
-})();
-
-const Player = (name, symbol) => { 
-  let _score = 0;
-  const getScore = () =>{
-    return _score;
-  };
-  const increaseScore = () => {
-    _score += 1;
-  };
-  return {name, symbol, increaseScore, getScore}
-};
-
-
-
-function takeTurn(cellId, index) {
-  if (continueGame) {
-    const cell = document.getElementById(cellId);
-    const turnSuccess = boardModule.fillBoardCell(index);
-    if (turnSuccess !== -1) {
-      if (boardModule.checkWin()) {
-        displayModule.showElement('win-screen');
-        boardModule.showMessageWinner();
-        displayModule.updateScores();
-        continueGame = false;
-      }
-      if (boardModule.checkDraw()) {
-        displayModule.showElement('win-screen');
-        displayModule.showMessageDraw();
-        continueGame = false;
-      }
-      cell.innerHTML = `<img class='board-img' src='img/tictactoe${turn}.svg'>`;
-      if (turn === 'X') {
-        turn = 'O';
-      } else {
-        turn = 'X';
+  const takeTurn = (cellId, index) => {
+    if (continueGame) {
+      const cell = document.getElementById(cellId);
+      const turnSuccess = fillBoardCell(index);
+      if (turnSuccess !== -1) {
+        if (checkWin()) {
+          displayModule.showElement('win-screen');
+          showMessageWinner();
+          displayModule.updateScores();
+          continueGame = false;
+        }
+        if (checkDraw()) {
+          displayModule.showElement('win-screen');
+          displayModule.showMessageDraw();
+          continueGame = false;
+        }
+        cell.innerHTML = `<img class='board-img' src='img/tictactoe${turn}.svg'>`;
+        if (turn === 'X') {
+          turn = 'O';
+        } else {
+          turn = 'X';
+        }
       }
     }
-  }
-}
+  };
 
-window.onload = function () {
+  return {
+    checkWin, fillBoardCell, checkDraw, resetGame, newGame, startGame, showMessageWinner, takeTurn,
+  };
+})();
+
+window.onload = function loadGame() {
   displayModule.hideElement('board');
   displayModule.showElement('players');
-  displayModule.hideElement('reset-top-button');  
+  displayModule.hideElement('reset-top-button');
 
   const newGameButton = document.getElementById('new-game-button');
   displayModule.addOnClickEvent(newGameButton, boardModule.newGame);
